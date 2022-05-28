e27e7fbdb25fcd00c9c8d533f84171ed65ee79f7
Tiago Web <tiagoweb@hotmail.com>
2022-05-28 11:13:03 -0400

    add componente product item na list product

--- lib/pages/products_overview.dart
+++ lib/pages/products_overview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -16,7 +17,7 @@ class ProductsOverviewPage extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: GridView.builder(
           itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
