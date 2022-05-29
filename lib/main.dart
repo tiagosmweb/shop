3456b59ba0c37d19b275cf61a7af5d24a40489f0
Tiago Web <tiagoweb@hotmail.com>
2022-05-29 06:18:29 -0400

    add provider main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_details.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'pages/products_overview.dart';
@@ -13,17 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-                primarySwatch: Colors.purple, accentColor: Colors.deepOrange)
-            .copyWith(secondary: Colors.deepOrange),
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+                  primarySwatch: Colors.purple, accentColor: Colors.deepOrange)
+              .copyWith(secondary: Colors.deepOrange),
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewPage(),
+        routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDatailsPage()},
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDatailsPage()},
-      debugShowCheckedModeBanner: false,
     );
   }
 }
