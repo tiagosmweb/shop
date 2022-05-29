1a1bbc30b8325bfc2ca0f380f8c191c5a611c968
Tiago Web <tiagoweb@hotmail.com>
2022-05-29 06:17:58 -0400

    refactory product grid e add provider

--- lib/pages/products_overview.dart
+++ lib/pages/products_overview.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyPoducts;
-  ProductsOverviewPage({Key? key}) : super(key: key);
+  const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +12,7 @@ class ProductsOverviewPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ),
+        child: ProductGrid(),
       ),
     );
   }
