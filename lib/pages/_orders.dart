39fe3f706f90339fd491006d495a20534db6dee4
Tiago Web <tiagoweb@hotmail.com>
2022-06-02 09:50:15 -0400

    bkp list order state

---
+++ lib/pages/_orders.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/app_drawer.dart';
+import 'package:shop/components/order.dart';
+import 'package:shop/models/order_list.dart';
+
+class OrdersPage extends StatefulWidget {
+  const OrdersPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((value) => setState(() => _isLoading = false));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Pedidos'),
+      ),
+      drawer: const AppDrawer(),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orders.itemsCount,
+              itemBuilder: (ctx, i) => OrderWidget(
+                order: orders.items[i],
+              ),
+            ),
+    );
+  }
+}
