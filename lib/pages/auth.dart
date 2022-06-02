6e9392821301e0a27d93ee37984dcbda8ee97090
Tiago Web <tiagoweb@hotmail.com>
2022-06-02 11:40:27 -0400

    update page auth

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -15,13 +18,45 @@ class AuthPage extends StatelessWidget {
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(215, 188, 117, 0.9),
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
           SizedBox(
             width: double.infinity,
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 70,
+                  ),
+                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Color.fromRGBO(0, 0, 0, 0.259),
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: const Text(
+                    'Minha Loja',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontFamily: 'Anton',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const AuthForm(),
+              ],
             ),
           ),
         ],
