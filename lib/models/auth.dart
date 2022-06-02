5ae784364acaf4dc274f8139f1f621c8ff9f2c2a
Tiago Web <tiagoweb@hotmail.com>
2022-06-02 16:38:04 -0400

    add auth e register firebase

---
+++ lib/models/auth.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop/exceptions/auth_exception.dart';
+
+class Auth with ChangeNotifier {
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyDc2VvWBDJdGM8hB7BOwk-5jK1jbWqpcHI';
+
+    final resp = await http.post(
+      Uri.parse(url),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+
+    final body = jsonDecode(resp.body);
+
+    if (body['error'] != null) {
+      throw AuthException(body['error']['message']);
+    }
+
+    print(body);
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+}
