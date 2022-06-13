a4e79bfe0acfcd120fcbde9468348cf033347296
Tiago Web <tiagoweb@hotmail.com>
2022-06-13 09:24:10 -0400

    ajuste animation

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -50,7 +50,7 @@ class _AuthFormState extends State<AuthForm>
         curve: Curves.linear,
       ),
     );
-    _heightAnimation?.addListener(() => setState(() {}));
+    // _heightAnimation?.addListener(() => setState(() {}));
   }
 
   @override
@@ -128,11 +128,15 @@ class _AuthFormState extends State<AuthForm>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        // height: _isLogin() ? 310 : 400,
-        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
-        width: deviceSize.width * 0.90,
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (ctx, childForm) => Container(
+          padding: const EdgeInsets.all(16),
+          // height: _isLogin() ? 310 : 400,
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
+          width: deviceSize.width * 0.90,
+          child: childForm,
+        ),
         child: Form(
           key: _formKey,
           child: Column(
